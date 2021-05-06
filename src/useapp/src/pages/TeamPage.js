184a3bf94f8db00e8e5cd7592b49bebde1710394
snehilsharma05 <snehilsharma05@gmail.com>
2021-05-06 20:05:19 +0530

    Added React Team Page

---
+++ src/useapp/src/pages/TeamPage.js
@@ -0,0 +1,26 @@
+import {React, useEffect, useState} from "react";
+import { MatchDetailCard } from "../components/MatchDetailCard";
+import { MatchSmallCard } from "../components/MatchSmallCard";
+
+export const TeamPage = () =>{
+    
+    const [team,setTeam] = useState({matches:[]});
+    useEffect(()=>{
+        const fetchMatches = async () =>{
+            const response = await fetch('http://localhost:8080/team/Mumbai%20Indians');
+            const data = await response.json();
+            console.log(data);
+            setTeam(data);
+        }
+        fetchMatches();
+    },[]);
+    if(!team)
+    return null;
+    return(
+       <div className="TeamPage">
+           <h1>{team.teamName}</h1>
+           <MatchDetailCard match={team.matches[0]}/>
+           {team.matches.slice(1).map(match => <MatchSmallCard match={match}/>)}
+       </div> 
+    );
+}
