c11c45fba1eaa812a078d417dd3ca9102c942e48
snehilsharma05 <snehilsharma05@gmail.com>
2021-05-07 19:09:59 +0530

    Added UI element for TeamPage

--- src/useapp/src/pages/TeamPage.js
+++ src/useapp/src/pages/TeamPage.js
@@ -1,26 +1,28 @@
 import {React, useEffect, useState} from "react";
+import { useParams } from "react-router-dom";
 import { MatchDetailCard } from "../components/MatchDetailCard";
 import { MatchSmallCard } from "../components/MatchSmallCard";
 
 export const TeamPage = () =>{
     
     const [team,setTeam] = useState({matches:[]});
+    const {teamName} = useParams();
     useEffect(()=>{
         const fetchMatches = async () =>{
-            const response = await fetch('http://localhost:8080/team/Mumbai%20Indians');
+            const response = await fetch(`http://localhost:8080/team/${teamName}`);
             const data = await response.json();
             console.log(data);
             setTeam(data);
         }
         fetchMatches();
-    },[]);
-    if(!team)
-    return null;
+    },[teamName]);
+    if(!team || !team.teamName)
+    return <h1>Team Not Found!</h1>;
     return(
        <div className="TeamPage">
            <h1>{team.teamName}</h1>
-           <MatchDetailCard match={team.matches[0]}/>
-           {team.matches.slice(1).map(match => <MatchSmallCard match={match}/>)}
+           <MatchDetailCard teamName = {team.teamName} match={team.matches[0]}/>
+           {team.matches.slice(1).map(match => <MatchSmallCard teamName = {team.teamName} match={match}/>)}
        </div> 
     );
 }
