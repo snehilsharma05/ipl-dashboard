37d6cc105e035ce5d3464bbb20e28a9af8e38bdb
snehilsharma05 <snehilsharma05@gmail.com>
2021-05-15 19:19:20 +0530

    Added YearSelector Compo

--- src/useapp/src/pages/MatchPage.js
+++ src/useapp/src/pages/MatchPage.js
@@ -2,7 +2,8 @@ import {React, useEffect, useState} from "react";
 import { useParams } from "react-router-dom";
 import { MatchDetailCard } from "../components/MatchDetailCard";
 import { MatchSmallCard } from "../components/MatchSmallCard";
-
+import { YearSelector } from "../components/YearSelector";
+import './MatchPage.scss';
 export const MatchPage = () =>{
     
     const [ matches, setMatches ] = useState([]);
@@ -16,12 +17,20 @@ export const MatchPage = () =>{
             setMatches(data);
         }
         fetchMatches();
-    },[]);
+    },[teamName,year]);
 
     return(
        <div className="MatchPage">
-           <h1>Match page</h1>
+           <div className="year-selector">
+               <h3>Select Year</h3>
+                <YearSelector teamName= {teamName}/>
+            </div>
+
+           <div>
+           <h1 className="page-heading">{teamName} matches in {year}</h1>
            {matches.map(match => <MatchDetailCard teamName = {teamName} match={match}/>)}
+           </div>
+
        </div> 
     );
 }
